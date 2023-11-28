aeb4b2228f00a6bcee62a54553cf24b375555bb7
hz <13457698+hozzjss@users.noreply.github.com>
2023-11-28 12:47:38 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <video
+    src="/gallery-gradient.mp4"
+    muted
+    playsinline
+    loop
+    autoplay
+    class="min-w-full min-h-full object-cover fixed"
+  ></video>
+</template>
+
+<style scoped></style>
