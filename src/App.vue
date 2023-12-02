f8440dec1c923380670a5cce55b9f82a304cf9ee
hz <13457698+hozzjss@users.noreply.github.com>
2023-12-02 18:47:37 +0200

    Update font to Menoe Grotesque, add gallery header and footer, update tailwind config
    This commit updates the font family of the website to Menoe Grotesque by
    adding it to the Tailwind CSS configuration file. It also modifies the
    App.vue component to include a new header with the gallery name and
    date, as well as a footer with additional information about the event.
    The video element is now wrapped in a div container to allow for more
    flexibility in positioning and layering elements on top of it.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <script setup lang="ts"></script>
 
 <template>
-  <video
-    src="/gallery-gradient.mp4"
-    muted
-    playsinline
-    loop
-    autoplay
-    preload="auto"
-    class="min-w-full min-h-full object-cover fixed"
-  ></video>
+  <div class="relative h-screen w-screen">
+    <video
+      src="/gallery-gradient.mp4"
+      muted
+      playsinline
+      loop
+      autoplay
+      preload="auto"
+      class="min-w-full min-h-full object-cover fixed z-0"
+    ></video>
+
+    <div
+      class="w-full h-full z-10 relative flex flex-col items-center justify-center"
+    >
+      <div class="relative">
+        <h1
+          class="absolute -top-16 uppercase sm:tracking-[1em] text-white text-4xl w-full text-center"
+        >
+          Gallery
+        </h1>
+        <img
+          src="./assets/images/logo-white.png"
+          alt=""
+          class="w-3/4 mx-auto max-w-[24rem] sm:w-full mt-8 hover:opacity-0 transition-all ease-in"
+        />
+      </div>
+      <div
+        class="fixed bottom-0 w-full flex justify-between pb-10 text-white text-xl px-4 sm:px-12 flex-wrap"
+      >
+        <div class="capitalize">Dec 2023</div>
+        <div class="capitalize">Miami art basel</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
