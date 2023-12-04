5302f42e7363b2c6e0643094a7f079a79e23650f
hz <13457698+hozzjss@users.noreply.github.com>
2023-12-04 16:13:45 +0200

    Update body class and add instructions for BTC wallpaper

--- src/pages/Miami.vue
+++ src/pages/Miami.vue
@@ -2,8 +2,7 @@
 <template>
     <main class="w-full bg-black text-white">
         <header class="px-6 relative">
-            <div class="w-full flex flex-row justify-between  border-b border-solid  border-gray-700 pb-20 pt-6 ">
-
+            <div class="w-full flex flex-row justify-between border-b border-solid border-gray-700 pb-20 pt-6">
                 <div class="basis-3/5 max-w-[280px]">
                     <img src="../assets/images/logo-white.png" alt="Logo" />
                 </div>
@@ -16,14 +15,34 @@
                 <span>Wallpaper</span>
             </div>
         </header>
-        <section class="w-full p-14 pb-0">
-            <div class="text-4xl italic relative w-max">
+        <section class="w-full pt-16 p-6 sm:p-14 pb-0">
+            <div class="text-4xl italic relative w-max mb-10">
                 <h3 class="text-lg">Thank you for visiting the</h3>
                 <hr class="bottom-4 absolute w-full border-t-[3px] z-0" />
-                <h1 class="text-center">
-                    BEAR MARKET
-                </h1>
+                <h1 class="text-center">BEAR MARKET</h1>
             </div>
+            <p class="text-gray-500 italic">
+                The exhibition is designed as an immersive experience for the community
+                to showcase the new Bitcoin Ordinals technology.
+            </p>
+            <p class="mt-10 text-gray-500 italic">
+                To get a free BTC wallpaper on mobile:
+            </p>
+
+            <ol class="mt-8 flex flex-col gap-2">
+                <li>1. Download & install <a href="">Xverse</a> wallet</li>
+                <li>
+                    2. Go to the browser section -
+                    <img src="../assets/images/world_icon.png" alt="" class="h-4 inline" />
+                </li>
+                <li>
+                    3. Enter URL on top:
+                    <a class="inline underline underline-offset-4 italic" href="https://nome.gallery/miami">
+                        nome.gallery/miami
+                    </a>
+                </li>
+                <li>4. Proceed with the mint in the wallet</li>
+            </ol>
         </section>
     </main>
 </template>
